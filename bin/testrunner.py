bf80e7ed7f1260f7f751fc2d1dbaf6067781028f
Justin <justin@pnnl.gov>
2023-09-13 09:38:07 -0700

    Updated testrunner for python3 and to allow splitting into test groups

--- bin/testrunner.py
+++ bin/testrunner.py
@@ -1,6 +1,11 @@
 from website_monitoring import TestSuite, TestGenerator
-import os, sys, unittest, json
+import os, sys, unittest, json, argparse
 import splunklib.client as client
+
+parser = argparse.ArgumentParser(description='Build a Selenium test suite from a saved list of test cases')
+parser.add_argument('--offset', type=int, default=0, help="Start position. Default is 0.")
+parser.add_argument('--groups', type=int, default=1, help="Number of groups to break the list into. Default is 1.")
+args = parser.parse_args()
 
 if __name__ == "__main__":
  
@@ -26,12 +31,20 @@ if __name__ == "__main__":
     f = open(outputfile, 'w')
     f.close()
 
-    for test in testData:
+    for test in testData[args.offset::args.groups]:
         if str(test.get('ENABLED', False)).lower() in ['true', '1']:
             # Convert TESTS to list if stored as a string
-            if type(test['TESTS']) is not list:
+            if not isinstance(test['TESTS'], list):
                 test['TESTS'] = json.loads(test['TESTS'])
             count = count + 1
+
+            # DEV OPTIONS
+            # OVERRIDE BROWSER
+            # test['BROWSER'] = "Chrome"
+            
+            # DEBUG
+            # test['DEBUG'] = True
+
             test_name = 'test_%03d' % count
             test_case = TestGenerator(test)
             setattr(TestSuite, test_name, test_case)
@@ -45,6 +58,6 @@ if __name__ == "__main__":
             f = open(outputfile, 'a')
             f.write(json.dumps(test.test.results, sort_keys=True) + '\n')
             f.close()
-    
+
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSuite)
     testrunner = unittest.TextTestRunner(verbosity=0, resultclass=TextTestResultWithSuccesses).run(suite)
